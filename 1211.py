f73a36de219beac81f2b11363d5c296c3be4852b
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2022-02-21 19:29:28 +0600

    Train Swapping

---
+++ 1211.py
@@ -0,0 +1,12 @@
+while True:
+    try:
+        n = int(input())
+        x = []
+        for i in range(n):
+            x.append(input())
+        count = 0
+
+        print(count)
+
+    except EOFError:
+        break
